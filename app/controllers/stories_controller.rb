926519c969c3de4c5b18f3d44c3777d5ce8b6224
Megan Wong <meganwong117@gmail.com>
2021-06-23 15:56:26 +0100

    added column to stories

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,2 +1,47 @@
 class StoriesController < ApplicationController
+  before_action :set_story, only: [ :show, :edit, :destroy ]
+
+  def index
+    @stories = Story.all
+  end
+
+  def new
+    @story = Story.new
+  end
+
+  def create
+    @story = Story.new(story_params)
+    if @story.save
+      redirect_to story_path(@story)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @story = Story.find(params[:id])
+    @story.update(story_params)
+    redirect_to story_path(@story)
+  end
+
+  def destroy
+    @story.destroy
+    redirect_to stories_path
+  end
+
+  def show
+  end
+
+  private
+
+  def set_story
+    @story = Story.find(params[:id])
+  end
+
+  def story_params
+    params.require(:story).permit(:title, :content, :comments)
+  end
 end
