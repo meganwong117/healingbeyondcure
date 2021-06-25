0b00fa85551989a5b7a2d61652c8d9669d21a33b
Megan Wong <meganwong117@gmail.com>
2021-06-25 16:28:59 +0100

    index and show for community

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,11 +1,34 @@
 class QuestionsController < ApplicationController
   def index
     @questions = Question.all
-    @answers = @question.answers
   end
 
   def show
     @question = Question.find(params[:id])
+    @answers = @question.answers
+    @answer = Answer.new
+  end
+
+  def edit
+    @question = Question.find(params[:id])
+  end
+
+  def update
+    @question = Question.find(params[:id])
+  end
+
+  def create
+    @question = Question.new(question_params)
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def destroy
+    @question = Question.find(params[:id])
+    @question.destroy
+    redirect_to questions_path
   end
 
   private
